5cd678e0fb6d1b2f6523e83c8afd9dc5fabe3aa1
Iogann1978 <iogann1978@gmail.com>
2023-08-10 23:20:35 +0300

    delete dialog

--- src/app/delete-dialog/delete-dialog.component.ts
+++ src/app/delete-dialog/delete-dialog.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-delete-dialog',
   templateUrl: './delete-dialog.component.html',
   styleUrls: ['./delete-dialog.component.css']
 })
-export class DeleteDialogComponent {
+export class DeleteDialogComponent implements OnInit {
+
+  constructor(private dialogRef: MatDialogRef<DeleteDialogComponent>) { }
+
+  close(flag: boolean) {
+    this.dialogRef.close(flag);
+  }
+
+  ngOnInit(): void {
+  }
 
 }
